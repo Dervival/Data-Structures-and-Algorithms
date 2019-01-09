1fbf97602d658561961f8024280d1d69d0c951c9
Blaise Clarke <blaiserclarke@gmail.com>
2019-01-09 07:45:33 -0800

    Added tests for two examples given in the assignment

--- Challenges/arrayShift/arrayShift/arrayShift/Program.cs
+++ Challenges/arrayShift/arrayShift/arrayShift/Program.cs
@@ -9,9 +9,26 @@ namespace arrayShift
             Console.WriteLine("Hello World!");
         }
 
-        public static int TestLink()
+        //public static int TestLink()
+        //{
+        //    return 1;
+        //}
+
+        public static int[] InsertShiftArray(int[] baseArray, int addedInteger)
         {
-            return 1;
+
+            int[] returnArray = new int[baseArray.Length +1];
+            int middleIndex = (baseArray.Length + 1) / 2;
+            for(int i = 0; i < middleIndex; i++)
+            {
+                returnArray[i] = baseArray[i];
+            }
+            returnArray[middleIndex] = addedInteger;
+            for(int i = middleIndex; i < baseArray.Length; i++)
+            {
+                returnArray[i + 1] = baseArray[i];
+            }
+            return returnArray;
         }
     }
 }
--- Challenges/arrayShift/arrayShift/arrayShiftUnitTests/UnitTest1.cs
+++ Challenges/arrayShift/arrayShift/arrayShiftUnitTests/UnitTest1.cs
@@ -6,12 +6,11 @@ namespace arrayShiftUnitTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void PassesLinking()
-        {
-            Assert.Equal(1, Program.TestLink());
-            //int [] exampleArray
-        }
+        //[Fact]
+        //public void PassesLinking()
+        //{
+        //    Assert.Equal(1, Program.TestLink());
+        //}
 
         [Fact]
         public void PassesFirstExample()
@@ -21,9 +20,23 @@ namespace arrayShiftUnitTests
             int exampleInt = 5;
             int[] expectedOutput = { 2, 4, 5, 6, 8 };
             //act
-            int[] actualOutput = InsertShiftArray(exampleArray, exampleInt);
+            int[] actualOutput = Program.InsertShiftArray(exampleArray, exampleInt);
             //assert
             Assert.Equal(expectedOutput, actualOutput);
         }
+
+        [Fact]
+        public void PassesSecondExample()
+        {
+            //arrange
+            int[] exampleArray = { 4, 8, 15, 23, 42 };
+            int exampleInt = 16;
+            int[] expectedOutput = { 4, 8, 15, 16, 23, 42 };
+            //act
+            int[] actualOutput = Program.InsertShiftArray(exampleArray, exampleInt);
+            //assert
+            Assert.Equal(expectedOutput, actualOutput);
+        }
+
     }
 }
