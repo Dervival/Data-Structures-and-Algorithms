1c26634779943534390e1c0d9cdb66bcf32c28f1
Blaise Clarke <blaiserclarke@gmail.com>
2019-01-10 09:20:11 -0800

    Cleaned up debug console statements and enhanced comments, updated READMEs

--- Challenges/arrayBinarySearch/README.md
+++ Challenges/arrayBinarySearch/README.md
@@ -1,22 +1,25 @@
-# Insert and Shift an Array
+# Binary Search of a sorted Array
 
 ## Challenge
-Write a method called insertShiftArray which takes in an array and the value to be added. 
-Without utilizing any of the array manipulation methods available in C#, return an array with the 
-new value added at the middle index. 
+Write a method called BinarySearch which takes in a sorted array and the value to be found in the array. 
+Without utilizing any of the array search methods available in C#, return either the index where the element
+was found, or -1 if the element is not present in the array.
 
 ## Approach & Efficiency
 
-1. Big O Time = O(n) // Big O Space = O(n)
-	- As this implementation is written in C# and the size of the array changes, we must make a new array to be 
-	returned by the function; this requires a minimum of O(n) space for the algorithm, where n is the number of
-	elements in the initial array. Since we are copying over all elements from the input array, we will need to 
-	do n assignments from the original array, resulting in a minimum of O(n) time for the algorithm. 
+1.  This algorithm uses a recursive binary search approach to implement BinarySearch. This is accomplished by
+	the middle element of the sorted array to the target value, and either returning that index, searching the
+	rest of the array to the left of that index, or searching the rest of the array to the right of the index 
+	if the value of the middle element is equal to, greater than, or less than the target value respectively.
+
+2.	Big O Time = O(log(n)) // Big O Space = O(n)
+	- Recursive binary search halves the number of elements needed to be searched every time a search is made -
+	this means that as the number of elements of the array doubles, only one additional search is needed.
+	- This implementation requires a subarray to be created for every new search. Since there are no available 
+	methods in the C# language to create a subarray without creating a new array instance, at the worst case we 
+	need to instantiate (n/2) + (n/4) + (n/8)... => n new elements to use this algorithm. 
 
 ## Solution
 
-![Whiteboard for Insert and Shift an Array](../../assets/Challenge2_full.jpg)
+![Whiteboard for Insert and Shift an Array](../../assets/Challenge3_full.jpg)
 Full picture of whiteboard. Apologies for the low quality of image.
-
-![Algorithm for Insert and Shift an Array](../../assets/Challenge2_algorithm.jpg)
-Detail picture of algorithm from whiteboard. Apologies for the low quality of image.
--- Challenges/arrayBinarySearch/arrayBinarySearch/arrayBinarySearch/Program.cs
+++ Challenges/arrayBinarySearch/arrayBinarySearch/arrayBinarySearch/Program.cs
@@ -6,16 +6,15 @@ namespace arrayBinarySearch
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Barrage testing in console - make sure that each element can be found
             int[] testArray = new int[] { 4, 8, 15, 16, 23, 42, 99 };
-            //int target = 4;
             int foundIndex;
             for (int i = 0; i < testArray.Length; i++)
             {
                 foundIndex = BinarySearch(testArray, testArray[i]);
                 Console.WriteLine("The element " + testArray[i] + " was found in the array at index " + foundIndex);
             }
-
+            //Make sure that an element that can't be found returns -1
             foundIndex = BinarySearch(testArray, 222);
             Console.WriteLine("The element 222 was found in the array at index " + foundIndex);
         }
@@ -29,11 +28,12 @@ namespace arrayBinarySearch
             }
             //Binary search - split the array into left, middle, and right; test the value in the middle against the target, then recurse if not equal
             int mid = (sortedArray.Length) / 2;
-            Console.WriteLine("mid is now " + mid);
+            //if mid is outside of the bounds of the array, the element cannot be found
             if (mid < 0 || mid > sortedArray.Length - 1)
             {
                 return -1;
             }
+            //if the target is found at the middle index, return that index
             if (target == sortedArray[mid])
             {
                 return mid;
@@ -47,31 +47,19 @@ namespace arrayBinarySearch
                 {
                     leftSubArray[i] = sortedArray[i];
                 }
-                Console.Write("Calling BinarySearch([");
-                for(int i = 0; i < leftSubArray.Length; i++)
-                {
-                    Console.Write(leftSubArray[i] + ",");
-                }
-                Console.WriteLine("], " + target + ") ");
                 return BinarySearch(leftSubArray, target);
             }
             //If the middle of the array is smaller than the middle value, the value must be in the right side of the array if it's in the array
             else if(target > sortedArray[mid])
             {
                 //left.Length + mid.Length + right.Length = sortedArray.Length
-                //mid - 1     +      1     + right.Length = sortedArray.Length
-                //right.length = sortedArray.Length - mid
+                //mid     +      1     + right.Length = sortedArray.Length
+                //right.length = sortedArray.Length - mid - 1
                 int[] rightSubArray = new int[sortedArray.Length - mid - 1];
                 for(int i = 0; i < sortedArray.Length - mid - 1; i++)
                 {
                     rightSubArray[i] = sortedArray[i + mid + 1];
                 }
-                Console.Write("Calling BinarySearch([");
-                for (int i = 0; i < rightSubArray.Length; i++)
-                {
-                    Console.Write(rightSubArray[i] + ",");
-                }
-                Console.WriteLine("], " + target + ") ");
                 int offsetIndex = BinarySearch(rightSubArray, target);
                 if(offsetIndex < 0)
                 {
