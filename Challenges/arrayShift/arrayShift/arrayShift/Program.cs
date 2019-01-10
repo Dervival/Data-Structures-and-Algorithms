b1a18f7fad16e7518809183755a8c9e11732f5e3
Blaise Clarke <blaiserclarke@gmail.com>
2019-01-09 21:22:05 -0800

    Added missing comments to arrayShift

--- Challenges/arrayShift/arrayShift/arrayShift/Program.cs
+++ Challenges/arrayShift/arrayShift/arrayShift/Program.cs
@@ -7,6 +7,7 @@ namespace arrayShift
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            // Testing via console - sanity testing off of the second example given
             int[] exampleArr = { 4, 8, 15, 23, 42 };
             int exampleInt = 16;
 
@@ -19,17 +20,24 @@ namespace arrayShift
             }
 
         }
-
+        /// <summary>
+        /// Given an array of integers and a single, separate integer, return a single array where the separate integer has been inserted in the middle of the array, with values at indices greater than the middle being shifted back one index.
+        /// </summary>
+        /// <param name="baseArray"> An array of integers of length n. The array does not need to be sorted. An empty integer array ([]) is acceptable input. </param>
+        /// <param name="addedInteger"> A single integer. No restrictions on input.</param>
+        /// <returns name="returnArray"> An array of integers of length n+1.</returns>
         public static int[] InsertShiftArray(int[] baseArray, int addedInteger)
         {
-
+            // Declaration of return array; since another int is being inserted, the new array must be one index longer than the input array.
             int[] returnArray = new int[baseArray.Length +1];
             int middleIndex = (baseArray.Length + 1) / 2;
+            //Since we're inserting into the middle, we can just copy elements from the input array to the new array until we hit the insertion point
             for(int i = 0; i < middleIndex; i++)
             {
                 returnArray[i] = baseArray[i];
             }
             returnArray[middleIndex] = addedInteger;
+            //Since we're shifting the values instead overwriting one, an offset is needed when assigning values into the new array
             for(int i = middleIndex; i < baseArray.Length; i++)
             {
                 returnArray[i + 1] = baseArray[i];
