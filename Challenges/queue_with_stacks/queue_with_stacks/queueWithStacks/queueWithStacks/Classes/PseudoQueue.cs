47e2af0e4e35238c636e5bde99a30c2404fc696e
Blaise Clarke <blaiserclarke@gmail.com>
2019-01-22 17:19:52 -0800

    Implemented both enqueue and dequeue, added very basic test

--- Challenges/queue_with_stacks/queue_with_stacks/queueWithStacks/queueWithStacks/Classes/PseudoQueue.cs
+++ Challenges/queue_with_stacks/queue_with_stacks/queueWithStacks/queueWithStacks/Classes/PseudoQueue.cs
@@ -7,6 +7,43 @@ namespace queueWithStacks.Classes
 {
     public class PseudoQueue
     {
-        
+        /// <summary>
+        /// InStack - stack to enqueue to. Outside of methods, this should be the only stack that contains any nodes. Consider the Top reference of the InStack similar to the Rear reference of a traditional queue.
+        /// </summary>
+        public static Stack InStack = new Stack();
+
+        /// <summary>
+        /// OutStack - stack used for dequeueing only. This should be empty at all times except during the dequeueing method, where it acts as a container for the elements being popped off of the InStack to expose the bottom element. When the InStack is empty, consider the top reference of the OutStack similar to the Front reference of a traditional queue.
+        /// </summary>
+        public static Stack OutStack = new Stack();
+
+        /// <summary>
+        /// Adds a node to the top of the InStack - because we are only dequeueing from the OutStack after the InStack has fully been transferred to the OutStack, the top node of InStack will be the node furthest from the top of OutStack, meaning it will be popped last when dequeueing. 
+        /// </summary>
+        /// <param name="value">Value to enqueue into the pseudoqueue</param>
+        public void Enqueue(int value)
+        {
+            Node newNode = new Node(value);
+            InStack.Push(newNode);
+        }
+
+        public Node Dequeue()
+        {
+            //figure out a better way to work with the edge case of an empty pseudoQueue than returning null...
+            if (InStack.Peek() == null)
+            {
+                return null;
+            }
+            while(InStack.Peek() != null)
+            {
+                OutStack.Push(InStack.Pop());
+            }
+            Node temp = OutStack.Pop();
+            while(OutStack.Peek() != null)
+            {
+                InStack.Push(OutStack.Pop());
+            }
+            return temp;
+        }
     }
 }
--- Challenges/queue_with_stacks/queue_with_stacks/queueWithStacks/queueWithStacksUTs/UnitTest1.cs
+++ Challenges/queue_with_stacks/queue_with_stacks/queueWithStacks/queueWithStacksUTs/UnitTest1.cs
@@ -7,6 +7,14 @@ namespace queueWithStacksUTs
 {
     public class UnitTest1
     {
-      
+        [Fact]
+        public void BigTest()
+        {
+            PseudoQueue newPQ = new PseudoQueue();
+            newPQ.Enqueue(10);
+            newPQ.Enqueue(20);
+            Node testNode = newPQ.Dequeue();
+            Assert.True(10 == testNode.Value);
+        }
     }
 }
