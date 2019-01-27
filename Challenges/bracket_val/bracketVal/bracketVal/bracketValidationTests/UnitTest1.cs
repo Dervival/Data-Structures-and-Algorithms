76e954e527106a9e1c4ab9f51e7a8093298002ab
Blaise Clarke <blaiserclarke@gmail.com>
2019-01-27 12:31:40 -0800

    Added unit tests for TDD of bracket validation algorithm

--- Challenges/bracket_val/bracketVal/bracketVal/bracketValidationTests/UnitTest1.cs
+++ Challenges/bracket_val/bracketVal/bracketVal/bracketValidationTests/UnitTest1.cs
@@ -6,9 +6,90 @@ namespace bracketValidationTests
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void EmptyStringIsConsideredBalanced()
         {
+            string emptyString = "";
+            Assert.True(MultiBracketValidation(emptyString));
+        }
 
+        [Theory]
+        [InlineData("()")]
+        [InlineData("[]")]
+        [InlineData("{}")]
+        public void SinglePairOfBracketsIsBalanced(string testString)
+        {
+            Assert.True(MultiBracketValidation(testString));
+        }
+
+        [Theory]
+        [InlineData("()[]")]
+        [InlineData("[]{}")]
+        [InlineData("{}[]()")]
+        public void MultipleIsolatedPairsOfBracketsAreBalanced(string testString)
+        {
+            Assert.True(MultiBracketValidation(testString));
+        }
+
+        [Theory]
+        [InlineData("((()))")]
+        [InlineData("({[]})")]
+        [InlineData("[({})]")]
+        public void MultipleNestedPairsOfBracketsAreBalanced(string testString)
+        {
+            Assert.True(MultiBracketValidation(testString));
+        }
+
+        [Theory]
+        [InlineData("{}")]
+        [InlineData("{}(){}")]
+        [InlineData("()[[Extra Characters]]")]
+        [InlineData("(){}[[]]")]
+        [InlineData("{}{Code}{Fellows}(())")]
+        public void AllExampleTestsFromCanvasPass(string testString)
+        {
+            Assert.True(MultiBracketValidation(testString));
+        }
+
+        [Theory]
+        [InlineData("(")]
+        [InlineData("{")]
+        [InlineData("[")]
+        [InlineData(")")]
+        [InlineData("}")]
+        [InlineData("]")]
+        public void SingleUnbalancedBracketDoesNotReturnTrue(string testString)
+        {
+            Assert.False(MultiBracketValidation(testString));
+        }
+
+        [Theory]
+        [InlineData("([]")]
+        [InlineData("{{}")]
+        [InlineData("[()")]
+        [InlineData("{})")]
+        [InlineData("[]}")]
+        [InlineData("[]{}]")]
+        public void UnbalancedBracketsDoNotReturnTrue(string testString)
+        {
+            Assert.False(MultiBracketValidation(testString));
+        }
+
+        [Theory]
+        [InlineData("[(])")]
+        [InlineData("[({}])")]
+        [InlineData("(({{[[))]]}}")]
+        public void EntertwinedBracketPairsReturnFalse(string testString)
+        {
+            Assert.False(MultiBracketValidation(testString));
+        }
+
+        [Theory]
+        [InlineData("[({}]")]
+        [InlineData("(](")]
+        [InlineData("{(})")]
+        public void AllExampleTestsFromCanvasFail(string testString)
+        {
+            Assert.False(MultiBracketValidation(testString));
         }
     }
 }
