db714ec1cc11318e5db2f8ffd4575e29c7be54ca
Blaise Clarke <blaiserclarke@gmail.com>
2019-01-30 09:10:36 -0800

    Added initial version of BFPrint, need to test it in console

--- Challenges/breadthFirstTraversal/breadthFirst/breadthFirst/Program.cs
+++ Challenges/breadthFirstTraversal/breadthFirst/breadthFirst/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Tree.Classes;
+using StacksAndQueues.Classes;
 
 namespace breadthFirst
 {
@@ -7,6 +9,29 @@ namespace breadthFirst
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+
+        static void BreadthFirstPrint(BinaryTree<int> binaryTree)
+        {
+            if(binaryTree.Root == null)
+            {
+                return;
+            }
+            Queue<Tree.Classes.Node<int>> BreadthFirstQueue = new Queue<Tree.Classes.Node<int>>();
+            BreadthFirstQueue.Enqueue(binaryTree.Root);
+            while(BreadthFirstQueue.Peek() != null)
+            {
+                Tree.Classes.Node<int> dequeuedNode = BreadthFirstQueue.Dequeue().Value;
+                Console.WriteLine(dequeuedNode);
+                if(dequeuedNode.LeftChild != null)
+                {
+                    BreadthFirstQueue.Enqueue(dequeuedNode.LeftChild);
+                }
+                if (dequeuedNode.RightChild != null)
+                {
+                    BreadthFirstQueue.Enqueue(dequeuedNode.RightChild);
+                }
+            }
         }
     }
 }
