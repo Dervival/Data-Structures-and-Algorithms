e6eadd7fe426b611d020f605a980f564cc7a86a6
Blaise Clarke <blaiserclarke@gmail.com>
2019-01-31 11:00:43 -0800

    Added FindMaxValue in tree and node form

--- Challenges/findMaxValueBT/findMaxValue/findMaxValue/Program.cs
+++ Challenges/findMaxValueBT/findMaxValue/findMaxValue/Program.cs
@@ -1,12 +1,45 @@
 ﻿using System;
+using Tree.Classes;
 
 namespace findMaxValue
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+
+        static int FindMaxValue(BinaryTree<int> binaryTree)
+        {
+            if(binaryTree.Root == null)
+            {
+                return Int32.MinValue;
+            }
+            return FindMaxValue(binaryTree.Root);
+        }
+
+        static int FindMaxValue(Node<int> root)
+        {
+            int leftValue = Int32.MinValue;
+            if(root.LeftChild != null)
+            {
+                leftValue = FindMaxValue(root.LeftChild);
+            }
+            int rightValue = Int32.MinValue;
+            if (root.RightChild != null)
+            {
+                rightValue = FindMaxValue(root.RightChild);
+            }
+            int maxValue = root.Value;
+            if(maxValue < leftValue){
+                maxValue = leftValue;
+            }
+            if(maxValue < rightValue)
+            {
+                maxValue = rightValue;
+            }
+            return maxValue;
         }
     }
 }
